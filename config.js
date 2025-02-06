4144dd95dad1cb9bb256c2c0c0b3834748553b42
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2025-02-06 22:32:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'NKP_WIFI',
+    platform : 'arduino-esp32',
+    title : 'NKP_WIFI',
+    description : 'ชุดบอร์ดสำหรับการเรียนรู้ IoT,Smart Farm,โครงงานวิทยาศาสตร์\n',
+    author : 'PrinceBot',
+    website : 'https://www.princebotshop.com/',
+    email : 'PrinceBot@gmail.com',
+    git : 'https://github.com/PrinceBot-Ratthanin/NKP_WIFI',    
+    image : '/static/display.png',
+    version : '1.0.0'
+};
